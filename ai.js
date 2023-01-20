6f065bb2ac6458dd0c7e25e8679d2890cd5593af
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-01-20 19:22:25 +0000

    changed some things pt 2

--- ai.js
+++ ai.js
@@ -5,18 +5,42 @@ const configuration = new Configuration({
     apiKey: process.env.opaitoken,
 });
 const openai = new OpenAIApi(configuration);
+
+function isValIncase(astr, prompt) {
+  const isContained = astr.some(element => {
+    return element.toLowerCase() === prompt.toLowerCase();
+  });
+
+  return isContained;
+}
+
+async function createResp(prompt, isContinue = false) {
+  return await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt,
+    temperature: isContinue ? 0.7 : 0,
+    max_tokens: isContinue ? 512 : 256,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+}
+
 async function ask(prompt) {
-    const response = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    });
-    const answer = response.data.choices[0].text;
-    return answer;
+  var response = ""; var answer = "";
+  var n = 0; var max = 1000;
+
+  if(isValIncase(["continue"], prompt)) {
+    while(n < max) {
+      response = await createResp(prompt, true);
+      answer += response.data.choices[0].text;
+    }  
+  } else {
+    response = await createResp(prompt, false);
+    answer = response.data.choices[0].text;
+  }
+
+  return answer;
 }
 //Ask an example question
 module.exports = {
