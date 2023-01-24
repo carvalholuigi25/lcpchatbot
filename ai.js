ce780408f8a8d203270e52c519d3b27313e912ae
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-01-24 14:27:47 +0000

    attempt to fix url in response data of ask function

--- ai.js
+++ ai.js
@@ -48,7 +48,7 @@ async function ask(prompt, type = "text") {
         answer += text;
       });
     } else {
-      answer = response.data.choices[0].text;
+      answer = response.data ? response.data.choices[0].text : null;
     }
   } else {
     if(prompt.indexOf("continue") !== -1) {
@@ -56,7 +56,7 @@ async function ask(prompt, type = "text") {
         answer += url;
       });
     } else {
-      answer = response.data.data[0].url;
+      answer = response.data ? response.data.data[0].url : null;
     }
   }
 
