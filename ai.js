43007a35a9ab2f3d38b6ff3c18c7f5d7728b8b03
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-01-24 11:31:50 +0000

    minor update 4

--- ai.js
+++ ai.js
@@ -14,21 +14,22 @@ function isValIncase(astr, prompt) {
   return isVal;
 }
 
-async function createResp(prompt) {
+async function createResp(prompt, isMaxTokens = false) {
   return await openai.createCompletion({
     model: "text-davinci-003",
     prompt,
-    temperature: 0.7,
-    max_tokens: 512,
+    temperature: 0.9,
+    max_tokens: isMaxTokens ? 2048 : 512,
     top_p: 1,
     frequency_penalty: 0.0,
-    presence_penalty: 0.0
+    presence_penalty: 0.0,
+    stop: null
   });
 }
 
 async function ask(prompt) {
   var answer = "";
-  var response = await createResp(prompt);
+  var response = await createResp(prompt, true);
   
   if(prompt.indexOf("continue") !== -1) {
     answer = `\nContinuing: `;
